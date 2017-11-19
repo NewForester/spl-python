40d88adaabd838e8f31d9503a57a99f32e0224c9
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-11-19 11:17:06 +0000

    Add exercism exercise #19, iteration #2 - Sieve

--- exercism/sieve.py
+++ exercism/sieve.py
@@ -5,23 +5,29 @@ def sieve(limit):
     """
     return list of primes up to limit using the Sieve of Eratosthenes
     """
-    def sieve_generator(limit):
-        """
-        generate primes up to limit using the Sieve of Eratosthenes
-        """
-        candidates = list(range(2, limit + 1))
+    if limit < 2:
+        return []
 
-        while candidates != []:
-            prime = candidates[0]
-            candidates = list(filter(lambda C: C % prime != 0, candidates[1:]))
-            yield prime
+    candidates = list(range(limit + 1))
+    candidates[0] = None
+    candidates[1] = None
 
-    return list(sieve_generator(limit))
+    prime = 2
+    while prime * prime <= limit:
+        if candidates[prime] != None:
+            multiple = prime + prime
+            while multiple <= limit:
+                candidates[multiple] = None
+                multiple += prime
+
+        prime = prime + 1
+
+    return [candidate for candidate in candidates if not candidate is None]
 
 ##
-## No solution was as neat as mine.  All involved a double loop.
+## This second iteration has no clever filtering of a list of potential primes.
+## It has just one list that is filtered at the very end.
 ##
-## Comments on his own work by mileskrains leads me to believe that
-## filtering on one list at the end instead of each iteration might
-## be faster if less clear.  I'm going write a second iteration.
+## Cutting out the list overhead will probably speed up the algorithm in any
+## language.  It has been claimed it may speed it up by an order of magnitude.
 ##
